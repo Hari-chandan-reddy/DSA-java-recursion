71c942cb4c6ee02af41e6d41682eb628ac48ee40
Pasula Hari Chandan Reddy <pharichandankkvs@gmail.com>
2026-03-24 19:04:52 +0530

    add passDecrement method to demonstrate pre vs post decrement in recursion.

--- src/com/dsa/recursion/RecursionDemo.java
+++ src/com/dsa/recursion/RecursionDemo.java
@@ -2,19 +2,60 @@ package com.dsa.recursion;
 
 public class RecursionDemo {
     public static void main(String[] args) {
-        print(1);
+        //printFor(5);
+        //printRev(5);
+        //printBoth(5);
+        passDecrement(5);
     }
 
-    public static void print(int n) {
-//        if(n == 6) {
+    static void passDecrement(int n) {
+        if(n == 0) {
+            return;
+        }
+
+        System.out.println(n);
+
+        // infinite loop(StackOverflowError): 'n--'(post decrement) passes 'n' value first and then subtracts by '1'.
+        //passDecrement(n--);
+
+        // No StackOverflowError: '--n'(pre decrement) subtracts 'n' by '1' and then passes 'n'.
+        passDecrement(--n);
+    }
+
+     static void printFor(int n) {
+//        if(n == 0) {
 //            return;
 //        }
 
-        if(n == 5) {
+        if(n == 1) {
             System.out.println(n);
             return;
         }
         System.out.println(n);
-        print(n + 1);
+        printFor(n - 1);
+    }
+
+    static void printRev(int n) {
+        if(n == 1) {
+            System.out.println(n);
+            return;
+        }
+        printRev(n - 1);
+        System.out.println(n);
+    }
+
+    static void printBoth(int n) {
+//        if(n == 1) {
+//            System.out.println(n);
+//            return;
+//        }
+
+        if(n == 0) {
+            return;
+        }
+
+        System.out.println(n);
+        printBoth(n - 1);
+        System.out.println(n);
     }
 }
